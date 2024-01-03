483c7e7da45ec77f30df62526211a0519f528b4a
amine.elalame <elm.dev.code@gmail.com>
2024-01-03 09:46:09 +0100

    feat(Java-TU-TDD-Exercises): TP 1 - Exercise 2 done.

---
+++ src/main/test/fr/cda/TP1/exercice2/TtcTest.java
@@ -0,0 +1,47 @@
+package fr.cda.TP1.exercice2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TtcTest {
+    private Ttc ttc;
+
+    @BeforeEach
+    public void setUp() {
+        ttc = new Ttc();
+    }
+
+    @ParameterizedTest
+    @DisplayName("Usage normal de la methode HtToTtc")
+    @CsvSource({"10", "20", "50"})
+    public void testHtToTtc(int prixHt) {
+
+        double expected = prixHt * ttc.TVA;
+        double result = ttc.HtToTtc(prixHt);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Le prix HT est negatif")
+    @CsvSource({"-10, -20, -50"})
+    public void testHtToTtcPrixHtNegatif(int prixHt) {
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> ttc.HtToTtc(prixHt));
+    }
+
+    @ParameterizedTest
+    @DisplayName("le prix HT est nul")
+    @ValueSource(ints = {0})
+    public void testHtToTtcPrixHtNul(int prixHt) {
+
+        double expected = 0;
+        double result = ttc.HtToTtc(prixHt);
+        assertEquals(expected, result);
+    }
+}
