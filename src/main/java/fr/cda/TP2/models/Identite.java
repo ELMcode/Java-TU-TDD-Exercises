7d21d82e21c4d50978485679c6966146c6778723
amine.elalame <elm.dev.code@gmail.com>
2024-01-04 10:24:38 +0100

    feat(main/java/fr/cda/TP2/models): Add 'Identite' class :sparkles:

--- src/main/java/fr/cda/TP2/models/Identite.java
+++ src/main/java/fr/cda/TP2/models/Identite.java
@@ -1,4 +1,37 @@
 package fr.cda.TP2.models;
 
 public class Identite {
+    private String nom;
+    private String prenom;
+    private Adresse adresse;
+
+    public Identite(String nom, String prenom, Adresse adresse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
 }
