df495ca79dd3adeb229eba170d36b352a9f686cb
amine.elalame <elm.dev.code@gmail.com>
2024-01-04 13:56:34 +0100

    test(main/test/fr/cda/TP2/models): Add SalaireTest class :white_check_mark:

--- src/main/test/fr/cda/TP2/models/SalaireTest.java
+++ src/main/test/fr/cda/TP2/models/SalaireTest.java
@@ -1,7 +1,25 @@
 package fr.cda.TP2.models;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
-class SalaireTest {
+public class SalaireTest {
 
+    @ParameterizedTest
+    @DisplayName("Testing 'Payer' with hours worked lower than 151.67")
+    @CsvSource({"10.5, 115, 1207.5"})
+    public void testPayerLower(double tauxHoraire, double heuresTravaillees, double expectedSalaire) {
+        Salaire salaire = new Salaire(tauxHoraire);
+        assertEquals(expectedSalaire, salaire.payer(heuresTravaillees));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Testing 'Payer' with hours worked higher than 151.67")
+    @CsvSource({"10.5, 160, 1701.86625"})
+    public void testPayerHigher(double tauxHoraire, double heuresTravaillees, double expectedSalaire) {
+        Salaire salaire = new Salaire(tauxHoraire);
+        assertEquals(expectedSalaire, salaire.payer(heuresTravaillees));
+    }
 }
