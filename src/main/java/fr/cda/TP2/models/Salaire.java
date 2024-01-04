058b2b97c707edc56bfb5863238453f8a52e97c9
amine.elalame <elm.dev.code@gmail.com>
2024-01-04 11:21:12 +0100

    feat(main/java/fr/cda/TP2/models): Add 'Salaire' class :sparkles:

--- src/main/java/fr/cda/TP2/models/Salaire.java
+++ src/main/java/fr/cda/TP2/models/Salaire.java
@@ -1,4 +1,34 @@
 package fr.cda.TP2.models;
 
 public class Salaire {
+    private double tauxHoraire;
+
+    public Salaire(double tauxHoraire) {
+        this.tauxHoraire = tauxHoraire;
+    }
+
+    public double getTauxHoraire() {
+        return tauxHoraire;
+    }
+
+    public void setTauxHoraire(double tauxHoraire) {
+        this.tauxHoraire = tauxHoraire;
+    }
+
+    /**
+     * Calculates the salary based on the number of hours worked
+     * @param  heuresTravaillees the number of hours worked
+     * @return the calculated salary
+     */
+    public double payer(int heuresTravaillees) {
+        double salaire = 0;
+        if (heuresTravaillees <= 151.67) {
+            salaire = tauxHoraire * heuresTravaillees;
+        } else {
+            double heuresNormales = 151.67;
+            double heuresSupp = heuresTravaillees - heuresNormales;
+            salaire = (tauxHoraire * heuresNormales) + (tauxHoraire * 1.25 * heuresSupp);
+        }
+        return salaire;
+    }
 }
