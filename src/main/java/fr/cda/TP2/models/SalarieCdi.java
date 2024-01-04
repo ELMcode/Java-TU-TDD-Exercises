cda87825ccc40c1f3693c68640dd22a72002f552
amine.elalame <elm.dev.code@gmail.com>
2024-01-04 11:57:00 +0100

    feat(main/java/fr/cda/TP2/models): Add 'SalarieCdi' class :sparkles:

--- src/main/java/fr/cda/TP2/models/SalarieCdi.java
+++ src/main/java/fr/cda/TP2/models/SalarieCdi.java
@@ -1,4 +1,7 @@
 package fr.cda.TP2.models;
 
-public class SalarieCdi {
+public class SalarieCdi extends Salarie {
+    public SalarieCdi(Salaire salaire, Identite identite) {
+        super(salaire, identite);
+    }
 }
