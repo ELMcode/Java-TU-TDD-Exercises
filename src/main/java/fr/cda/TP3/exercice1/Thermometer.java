e7bafe5d3df65fb5156f1d2625781417bc25f682
amine.elalame <elm.dev.code@gmail.com>
2024-01-05 15:21:23 +0100

    test(main/test/fr/cda/TP3/exercice1): empty array returns zero after implementation :white_check_mark:

--- src/main/java/fr/cda/TP3/exercice1/Thermometer.java
+++ src/main/java/fr/cda/TP3/exercice1/Thermometer.java
@@ -2,4 +2,11 @@ package fr.cda.TP3.exercice1;
 
 public class Thermometer {
 
+    public int closestToZero(int[] temperatures) {
+
+        if (temperatures.length == 0) {
+            return 0;
+        }
+        return 0;
+    }
 }
