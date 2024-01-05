ac720be2cca141677be81fabc5effb15a6c7ec14
amine.elalame <elm.dev.code@gmail.com>
2024-01-05 15:08:01 +0100

    chore(main/java/fr/cda/TP3): Init Thermometer Class & ThermometerTest for TDD

---
+++ src/main/java/fr/cda/TP3/exercice1/Thermometer.java
@@ -0,0 +1,5 @@
+package fr.cda.TP3.exercice1;
+
+public class Thermometer {
+
+}
---
+++ src/main/test/fr/cda/TP3/exercice1/ThermometerTest.java
@@ -0,0 +1,7 @@
+package fr.cda.TP3.exercice1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ThermometerTest {
+
+}
